82acb68a3c964ac5e95979c16d699825a2c8f7b6
deployfriday <walker.echo@proton.me>
2026-04-22 15:11:17 +0300

    feat: add database schema

---
+++ todo/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    title        TEXT NOT NULL,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    completed    BOOLEAN   NOT NULL DEFAULT FALSE,
+    completed_at TIMESTAMP
+);
